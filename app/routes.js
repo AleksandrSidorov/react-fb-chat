b96333e8aae1a999e27d72438eae460dd3b599ff
Aleksandr Sidorov <shtil80@gmail.com>
2017-03-14 17:05:48 +0700

    root route leads to chat panel

--- app/routes.js
+++ app/routes.js
@@ -22,7 +22,7 @@ export default function createRoutes(store) {
       name: 'home',
       getComponent(nextState, cb) {
         const importModules = Promise.all([
-          import('containers/HomePage'),
+          import('containers/ChatPanel'),
         ]);
 
         const renderRoute = loadModule(cb);
